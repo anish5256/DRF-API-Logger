5193c5644e62322941501079826930e2da00715b
anish <anish5256@gmail.com>
2024-05-29 15:08:51 +0530

    Modified Middleware to store 500 errors

--- drf_api_logger/middleware/api_logger_middleware.py
+++ drf_api_logger/middleware/api_logger_middleware.py
@@ -1,17 +1,16 @@
-import importlib
 import json
-import sys
 import time
-import uuid
 import re
-
 from django.conf import settings
 from django.urls import resolve
 from django.utils import timezone
-
+from django.http import HttpResponse
+from django.conf import settings
+import traceback
 from drf_api_logger import API_LOGGER_SIGNAL
 from drf_api_logger.start_logger_when_server_starts import LOGGER_THREAD
 from drf_api_logger.utils import get_headers, get_client_ip, mask_sensitive_data
+
 
 """
 File: api_logger_middleware.py
@@ -61,28 +60,16 @@ class APILoggerMiddleware:
                     settings.DRF_API_LOGGER_STATUS_CODES) is list:
                 self.DRF_API_LOGGER_STATUS_CODES = settings.DRF_API_LOGGER_STATUS_CODES
 
-        self.DRF_API_LOGGER_ENABLE_TRACING = False
-        self.DRF_API_LOGGER_TRACING_ID_HEADER_NAME = None
-        if hasattr(settings, 'DRF_API_LOGGER_ENABLE_TRACING'):
-            self.DRF_API_LOGGER_ENABLE_TRACING = settings.DRF_API_LOGGER_ENABLE_TRACING
-            if self.DRF_API_LOGGER_ENABLE_TRACING and hasattr(settings, 'DRF_API_LOGGER_TRACING_ID_HEADER_NAME'):
-                self.DRF_API_LOGGER_TRACING_ID_HEADER_NAME = settings.DRF_API_LOGGER_TRACING_ID_HEADER_NAME
-
-        self.tracing_func_name = None
-        if hasattr(settings, 'DRF_API_LOGGER_TRACING_FUNC'):
-            mod_name, func_name = settings.DRF_API_LOGGER_TRACING_FUNC.rsplit('.', 1)
-            mod = importlib.import_module(mod_name)
-            self.tracing_func_name = getattr(mod, func_name)
-
-        self.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE = -1
-        if hasattr(settings, 'DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE'):
-            if type(settings.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE) is int:
-                self.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE = settings.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE
-
-        self.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE = -1
-        if hasattr(settings, 'DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE'):
-            if type(settings.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE) is int:
-                self.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE = settings.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE
+    def get_api_uri(self, request):
+        if self.DRF_API_LOGGER_PATH_TYPE == 'ABSOLUTE':
+            api = request.build_absolute_uri()
+        elif self.DRF_API_LOGGER_PATH_TYPE == 'FULL_PATH':
+            api = request.get_full_path()
+        elif self.DRF_API_LOGGER_PATH_TYPE == 'RAW_URI':
+            api = request.get_raw_uri()
+        else:
+            api = request.build_absolute_uri()
+        return api
 
     def __call__(self, request):
 
@@ -104,39 +91,12 @@ class APILoggerMiddleware:
             if namespace in self.DRF_API_LOGGER_SKIP_NAMESPACE:
                 return self.get_response(request)
 
-            # Code to be executed for each request/response after
-            # the view is called.
-
             start_time = time.time()
-
-            headers = get_headers(request=request)
-            method = request.method
-
             request_data = ''
             try:
                 request_data = json.loads(request.body) if request.body else ''
-                if self.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE > -1:
-                    if sys.getsizeof(request_data) > self.DRF_API_LOGGER_MAX_REQUEST_BODY_SIZE:
-                        """
-                        Ignore the request body if larger then specified.
-                        """
-                        request_data = ''
             except:
                 pass
-
-            tracing_id = None
-            if self.DRF_API_LOGGER_ENABLE_TRACING:
-                if self.DRF_API_LOGGER_TRACING_ID_HEADER_NAME:
-                    tracing_id = headers.get(self.DRF_API_LOGGER_TRACING_ID_HEADER_NAME)
-                if not tracing_id:
-                    """
-                    If tracing is is not present in header, get it from function or uuid.
-                    """
-                    if self.tracing_func_name:
-                        tracing_id = self.tracing_func_name()
-                    else:
-                        tracing_id = str(uuid.uuid4())
-                request.tracing_id = tracing_id
 
             # Code to be executed for each request before
             # the view (and later middleware) are called.
@@ -146,46 +106,29 @@ class APILoggerMiddleware:
             if self.DRF_API_LOGGER_STATUS_CODES and response.status_code not in self.DRF_API_LOGGER_STATUS_CODES:
                 return response
 
+            # Code to be executed for each request/response after
+            # the view is called.
+
+            headers = get_headers(request=request)
+            method = request.method
+
             # Log only registered methods if available.
             if len(self.DRF_API_LOGGER_METHODS) > 0 and method not in self.DRF_API_LOGGER_METHODS:
                 return response
 
-            self.DRF_API_LOGGER_CONTENT_TYPES = [
-                "application/json",
-                "application/vnd.api+json",
-                "application/gzip",
-                "application/octet-stream",
-            ]
-            if hasattr(settings, "DRF_API_LOGGER_CONTENT_TYPES") and type(
-                settings.DRF_API_LOGGER_CONTENT_TYPES
-            ) in (list, tuple):
-                for content_type in settings.DRF_API_LOGGER_CONTENT_TYPES:
-                    if re.match(r"^application\/vnd\..+\+json$", content_type):
-                        self.DRF_API_LOGGER_CONTENT_TYPES.append(content_type)
+            if response.get('content-type') in ('application/json', 'application/vnd.api+json', 'application/gzip'):
 
-            if response.get("content-type") in self.DRF_API_LOGGER_CONTENT_TYPES:
                 if response.get('content-type') == 'application/gzip':
                     response_body = '** GZIP Archive **'
-                elif response.get('content-type') == 'application/octet-stream':
-                    response_body = '** Binary File **'
                 elif getattr(response, 'streaming', False):
                     response_body = '** Streaming **'
                 else:
-                    if type(response.content) is bytes:
+                    if type(response.content) == bytes:
                         response_body = json.loads(response.content.decode())
                     else:
                         response_body = json.loads(response.content)
-                if self.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE > -1:
-                    if sys.getsizeof(response_body) > self.DRF_API_LOGGER_MAX_RESPONSE_BODY_SIZE:
-                        response_body = ''
-                if self.DRF_API_LOGGER_PATH_TYPE == 'ABSOLUTE':
-                    api = request.build_absolute_uri()
-                elif self.DRF_API_LOGGER_PATH_TYPE == 'FULL_PATH':
-                    api = request.get_full_path()
-                elif self.DRF_API_LOGGER_PATH_TYPE == 'RAW_URI':
-                    api = request.get_raw_uri()
-                else:
-                    api = request.build_absolute_uri()
+
+                api = self.get_api_uri(request)
 
                 data = dict(
                     api=mask_sensitive_data(api, mask_api_parameters=True),
@@ -198,22 +141,55 @@ class APILoggerMiddleware:
                     execution_time=time.time() - start_time,
                     added_on=timezone.now()
                 )
-                if self.DRF_API_LOGGER_DATABASE:
-                    if LOGGER_THREAD:
-                        d = data.copy()
-                        d['headers'] = json.dumps(d['headers'], indent=4, ensure_ascii=False) if d.get('headers') else ''
-                        if request_data:
-                            d['body'] = json.dumps(d['body'], indent=4, ensure_ascii=False) if d.get('body') else ''
-                        d['response'] = json.dumps(d['response'], indent=4, ensure_ascii=False) if d.get('response') else ''
-                        LOGGER_THREAD.put_log_data(data=d)
-                if self.DRF_API_LOGGER_SIGNAL:
-                    if tracing_id:
-                        data.update({
-                            'tracing_id': tracing_id
-                        })
-                    API_LOGGER_SIGNAL.listen(**data)
+                self.save_data(self, data, request_data)
             else:
                 return response
         else:
             response = self.get_response(request)
         return response
+
+    def process_exception(self, request, exception):
+
+        message = None
+        if exception:
+            # Format your message here
+            message = "**{url}**\n\n{error}\n\n````{tb}````".format(
+                url=request.build_absolute_uri(),
+                error=repr(exception),
+                tb=traceback.format_exc()
+            )
+            print(message)
+            api = self.get_api_uri(request)
+
+            request_data = ''
+            try:
+                request_data = json.loads(request.body) if request.body else ''
+            except:
+                pass
+
+            data = dict(
+                api=mask_sensitive_data(api, mask_api_parameters=True),
+                headers=mask_sensitive_data(get_headers(request=request)),
+                body=mask_sensitive_data(request_data),
+                method=request.method,
+                client_ip_address=get_client_ip(request),
+                response=mask_sensitive_data(message),
+                status_code=500,
+                execution_time=0, #as this method run independant execution time is not there
+                added_on=timezone.now()
+            )
+            self.save_data(data, request_data)
+        return exception
+
+    def save_data(self, data, request_data):
+        if self.DRF_API_LOGGER_DATABASE:
+            if LOGGER_THREAD:
+                d = data.copy()
+                d['headers'] = json.dumps(d['headers'], indent=4, ensure_ascii=False)
+                if request_data:
+                    d['body'] = json.dumps(d['body'], indent=4, ensure_ascii=False)
+                d['response'] = json.dumps(d['response'], indent=4, ensure_ascii=False)
+                LOGGER_THREAD.put_log_data(data=d)
+        if self.DRF_API_LOGGER_SIGNAL:
+            API_LOGGER_SIGNAL.listen(**data)
+
